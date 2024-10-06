0ed9215add2e6ac53c14de22f45a39430f156f9c
Tony Chow <tonychow.0929@icloud.com>
2024-10-06 19:26:03 +0800

    Clean up videos frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useNavigate, useOutletContext } from 'react-router';
 import { NavLink } from 'react-router-dom';
@@ -20,7 +20,11 @@ const Item = styled.div`
     background-color: white;
 `;
 
-const CreateItem = styled(Item)`width: auto; display: inline-block`;
+const SmallItem = styled(Item)`
+    width: auto;
+    cursor: pointer;
+    display: inline-block;
+`;
 
 const LangButton = styled.button`
     background: transparent;
@@ -33,7 +37,7 @@ const LangButton = styled.button`
 
 function HomePage({ setLocale }) {
     const intl = useIntl();
-    const [downloadedList] = useVideoInfo();
+    const [downloadedList, setDownloadedList] = useVideoInfo();
     const navigate = useNavigate();
     const handleCreateVideo = useCallback(async (event) => {
         event.preventDefault();
@@ -45,17 +49,52 @@ function HomePage({ setLocale }) {
         navigate('/create');
     }, []);
 
+    const [cleanedVideoCount, setCleanedVideoCount] = useState(null);
+    const handleCleanVideos = useCallback(async () => {
+        try {
+            const res = await fetch('http://localhost:9000/garbage-videos', { method: 'delete' });
+            const { count } = await res.json();
+
+            setCleanedVideoCount(count);
+
+            const refetchRes = await fetch('http://localhost:9000/downloaded-videos');
+
+            if (refetchRes.ok) {
+                const { videos } = await refetchRes.json();
+
+                setDownloadedList(videos);
+            }
+        } catch {
+            setCleanedVideoCount(null);
+        }
+    }, []);
+
     return (
         <div>
             <H1>{intl.formatMessage({ id: 'homePage.title' })}</H1>
-            {downloadedList && <CreateItem key="create">
-                <NavLink
-                    to="/create"
-                    onClick={handleCreateVideo}
-                >
-                    {intl.formatMessage({ id: 'homePage.createButton.text' })}
-                </NavLink>
-            </CreateItem>}
+            <dialog open={typeof cleanedVideoCount === 'number'} onClose={() => { setCleanedVideoCount(null); }}>
+                <form method="dialog">
+                    <p>
+                        {intl.formatMessage({ id: 'homePage.cleanup.done' }, { count: cleanedVideoCount})}
+                    </p>
+                    <input type="submit" value={intl.formatMessage({ id: 'homePage.cleanup.close' })} />
+                </form>
+            </dialog>
+            {downloadedList && (
+                <>
+                    <SmallItem key="create">
+                        <NavLink
+                            to="/create"
+                            onClick={handleCreateVideo}
+                        >
+                            {intl.formatMessage({ id: 'homePage.createButton.text' })}
+                        </NavLink>
+                    </SmallItem>
+                    <SmallItem key="clean" onClick={handleCleanVideos}>
+                        {intl.formatMessage({ id: 'homePage.cleanButton.text' })}
+                    </SmallItem>
+                </>
+            )}
             {downloadedList?.map(({ url, createdAt, name }) => (
                 <Item key={url}>
                 <NavLink to={`/watch/${encodeURIComponent(createdAt)}`}>
